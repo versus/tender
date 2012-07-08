688c6c818977a2e297e5b0c595dce2e4bc926913
Valentin Nastenko <versus.ua@gmail.com>
2012-07-08 20:19:33 +0300

    main page, product page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,15 @@ class WelcomeController < ApplicationController
   def index
   	@items = Array.new
   	item,i=1444791,0
-  	while i<36																																		
+  	while i<38																																	
   		@items.push(item+i)
   		i=i+1
   	end
+  	#@items = @items.each_slice(5).to_a
 
+  end
+
+  def product
+  	@product = params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tender::Application.routes.draw do
   get "welcome/index"
   root :to => 'welcome#index'
+  match 'product/:id' => 'welcome#product'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
